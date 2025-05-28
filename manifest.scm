1c24603d7f0572ad853fbba148086d26e866c4d4
pma @ ikga <pma@rdorte.org>
2025-05-28 11:18:03 +0200

    Split manifest up into required and optional packages

--- manifest.scm
+++ manifest.scm
@@ -3,27 +3,41 @@
 ;; operations.
 (define tei-required-packages
   (list
+   ;; Required for ‘make html-web’ to succeed:
    "ant"
-   "bash"
+   "bash-minimal"
    "coreutils"
-   "findutils"
-   "gawk"
    "git"
-   "grep"
-   "less"
    "libxml2"
    "make"
-   ;; We need a version < 24, it turns out, because some security
-   ;; manager has been permanently disabled in v24 (see
-   ;; https://docs.oracle.com/en/java/javase/24/security/security-manager-is-permanently-disabled.html)
-   "openjdk@23.0.2"
-   "parallel"
+   "openjdk@23.0.2" ; see note below
    "perl"
    "sed"
+   "tar"))
+
+(define tei-useful-packages
+  (list
+   ;; These are not necessary but useful in general:
+   "findutils"
+   "gawk"
+   "grep"
+   "less"
+   "parallel"
    "tree"))
 
-(specifications->manifest tei-required-packages)
+;; Note to "openjdk@23.0.2": We need a version < 24, it turns out,
+;; because some security manager has been permanently disabled in v24
+;; (see
+;; https://docs.oracle.com/en/java/javase/24/security/security-manager-is-permanently-disabled.html).
 
-;; (packages->manifest tei-required-packages)
+;; TEIC’s Dockerfile has openjdk-17-jdk-headless, see
+;; https://github.com/TEIC/teidev-docker/blob/7606892c69e2ac24ca5a8fc6cd041487311c0d74/Dockerfile#L6
+
+;; Run as ‘TEISTUFF-MINIMAL-ONLY=0 guix’ to only use ‘tei-required-packages’
+
+(specifications->manifest
+ (append
+  tei-required-packages
+  tei-useful-packages))
 
 
